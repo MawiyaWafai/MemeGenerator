85ad41c0c7e4a9f8c5190873cd3e6008ada239f5
MawiyaWafai <mawiyawafaie@gmail.com>
2023-06-07 10:34:32 +0300

    Update

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import img from '../images/Vector.png'
+
+export default function Header() {
+    return (
+        <header>
+            <img src={img} alt="vector"/>
+        </header>
+    )
+}
